85db7af3b262609ec2de485513abb12d7b863648
Lillian Chernin <l.smith1654@gmail.com>
2018-01-04 22:53:52 -0600

    creates renderImage, creates routes for piano-chords

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ app.use(function(req, res, next) {
   next();
 });
 
+//creates 404 error message to send back to client when page does not exist
 const error404Message = "<h1 style=" + "border-bottom: 2px solid black"+ ">404 ERROR</h1><h3>Page not found!</h3><p>Sorry the requested URL does not exist.  Please check your URL or return to the home page.</p>";
+
+//creates img tag
+const renderImage = (imageUrl) => {
+  return "<img src=" + imageUrl + ">";
+}
 
 const comedians = [
   {id: 1, name: "Robin Williams"},
@@ -24,6 +30,42 @@ app.get('/comedians/:id', (request, response) => {
   }
   return response.send(error404Message);
 });
+
+const pianoChords = [
+  {id: 1, name: "C Major", triadNotes: ["c","e","g"], imageUrl: "https://i.imgur.com/hNKfOlb.png"},
+  {id: 2, name: "D Major", triadNotes: ["d","f#","a"], imageUrl: "https://i.imgur.com/LHRyfyC.png"},
+  {id: 3, name: "E Major", triadNotes: ["e","g#","b"], imageUrl: "https://i.imgur.com/JgUcq4s.png"},
+  {id: 4, name: "F Major", triadNotes: ["f","a","c"], imageUrl: "https://i.imgur.com/m6xKPgf.png"},
+  {id: 5, name: "G Major", triadNotes: ["g","b","d"], imageUrl: "https://i.imgur.com/h6i2a9b.png"},
+  {id: 6, name: "A Major", triadNotes: ["a","c#","e"], imageUrl: "https://i.imgur.com/rMznaNI.png"},
+  {id: 7, name: "B Major", triadNotes: ["b","d#","f#"], imageUrl: "https://i.imgur.com/0DaBniH.png"}
+]
+
+app.get('/piano-chords', (request, response) => {
+  response.send(pianoChords);
+})
+
+app.get('/piano-chords/:id', (request, response) => {
+  for (let i = 0; i < pianoChords.length; i++) {
+    //creates a lowercase single word string version of the pianoChord name
+    let chordName = pianoChords[i].name.toLowerCase().split(' ').join('');
+    if (Number(pianoChords[i].id) === Number(request.params.id) || chordName === (request.params.id).toLowerCase()) {
+      return response.send(pianoChords[i]);
+    }
+  }
+  return response.send(error404Message);
+})
+
+app.get('/piano-chords/:id/image', (request, response) => {
+  for (let i = 0; i < pianoChords.length; i++) {
+    let chordName = pianoChords[i].name.toLowerCase().split(' ').join('');
+    if (Number(pianoChords[i].id) === Number(request.params.id) || chordName === (request.params.id).toLowerCase()) {
+      return response.send(renderImage(pianoChords[i].imageUrl));
+    }
+  }
+  return response.send(error404Message);
+})
+
 
 app.listen(3000, () => {
     console.log("I am listening");
